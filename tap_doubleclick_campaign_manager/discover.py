ffafca8cc6b9af72cb6d6227ccf06039dcd84ceb
Andrew Madonna <andrew.w.madonna@gmail.com>
2018-06-13 23:57:43 -0400

    Catalog support and string reports config

--- tap_doubleclick_campaign_manager/discover.py
+++ tap_doubleclick_campaign_manager/discover.py
@@ -1,15 +1,40 @@
-## TODO: need to discover report columns
-## TODO: combine all report columns into one large table?
-## https://developers.google.com/doubleclick-advertisers/v3.1/dimensions
+from singer.catalog import Catalog, CatalogEntry, Schema
 
-def discover_streams(client):
-    streams = []
-    for s in STREAMS.values():
-        s = s(client)
-        streams.append({
-            'stream': s.name,
-            'tap_stream_id': s.name,
-            'schema': s.load_schema(),
-            'metadata': s.load_metadata()
-        })
-    return streams
+from tap_doubleclick_campaign_manager.schema import (
+    SINGER_REPORT_FIELD,
+    get_fields,
+    get_schema,
+    get_field_type_lookup
+)
+
+def discover_streams(service, config):
+    profile_id = config.get('profile_id')
+    reports = config.get('reports')
+
+    field_type_lookup = get_field_type_lookup()
+    catalog = Catalog([])
+
+    for report_config in reports:
+        report_id = report_config['report_id']
+        stream_name = report_config['stream_name']
+
+        report = (
+            service
+            .reports()
+            .get(profileId=profile_id,
+                 reportId=report_id)
+            .execute()
+        )
+
+        fieldmap = get_fields(field_type_lookup, report)
+        schema_dict = get_schema(stream_name, fieldmap)
+        schema = Schema.from_dict(schema_dict)
+
+        catalog.streams.append(CatalogEntry(
+            stream=stream_name,
+            tap_stream_id=stream_name,
+            key_properties=[],
+            schema=schema,
+        ))
+
+    return catalog.to_dict()
