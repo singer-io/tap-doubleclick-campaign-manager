7a88c76e26176a4e3e943a314aac56bc661bad09
Tushar Mittal <tushar.mittal@qlik.com>
2025-06-23 12:00:35 +0530

    extra installations removed

--- setup.py
+++ setup.py
@@ -12,15 +12,12 @@ setup(
     py_modules=["tap_doubleclick_campaign_manager"],
     install_requires=[
         "singer-python==6.1.1",
-        "pendulum==3.1.0",
         "google-api-python-client==2.172.0",
         "oauth2client==4.1.3"
     ],
     extras_require={
         "dev": [
             "beautifulsoup4==4.13.4",
-            "bs4==0.0.2",
-            "soupsieve==2.7",
             "pytest==8.4.1"
         ],
     },
