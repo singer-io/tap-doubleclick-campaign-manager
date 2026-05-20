b54bfd537c6df4156dde69810476c3b3f038503f
Sourabh Gandhi <sgandhi@talend.com>
2026-05-20 10:12:26 +0000

    fix: upgrade Google packages to resolve CVEs
    - Bump google-api-python-client from 2.193.0 to 2.196.0
    - Bump google-auth from 2.49.1 to 2.53.0
    - Fixes CVE-2026-44432 (urllib3 vulnerability)
    - Fixes CVE-2026-0994 (protobuf DoS vulnerability)
    - Version bumped to 1.5.1
    - All unit tests passing (114 tests)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tap-doubleclick-campaign-manager",
-    version="1.5.0",
+    version="1.5.1",
     description="Singer.io tap for extracting data from the DoubleClick for Campaign Managers API",
     author="Stitch",
     url="http://singer.io",
@@ -12,8 +12,8 @@ setup(
     py_modules=["tap_doubleclick_campaign_manager"],
     install_requires=[
         "singer-python==6.8.0",
-        "google-api-python-client==2.193.0",
-        "google-auth==2.49.1",
+        "google-api-python-client==2.196.0",
+        "google-auth==2.53.0",
         "backoff==2.2.1",
     ],
     extras_require={
